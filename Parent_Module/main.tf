649fa10a380829f881059b7d18ace4d3e4931646
Meharwan Singh <gistcs88@gmail.com>
2026-09-22 11:44:58 +0530

    Uncomment azurerm_container_registry module and role assignment

--- Parent_Module/main.tf
+++ Parent_Module/main.tf
@@ -9,16 +9,15 @@ module "azurerm_kubernetes_cluster" {
     depends_on = [module.azurerm_resource_group]
 }
 
-# module "azurerm_container_registry" {
-#     source = "../Child_Module/Azure_Container_Registry/Azurerm_Resource_Group"
-#   acr_registries = var.acr_registries
-#   depends_on = [ module.azurerm_resource_group ]
-# }
+ module "azurerm_container_registry" {
+     source = "../Child_Module/Azure_Container_Registry/Azurerm_Resource_Group"
+   acr_registries = var.acr_registries
+   depends_on = [ module.azurerm_resource_group ]
+ }
 
-
-# resource "azurerm_role_assignment" "aks_acr_pull" {
-#   principal_id = module.azurerm_kubernetes_cluster.kubelet_identity_object_id["aks1"]
-#   role_definition_name = "AcrPull"
-#   scope = module.azurerm_container_registry.acr_id["acr1"]
-#   skip_service_principal_aad_check = true
-# }
+ resource "azurerm_role_assignment" "aks_acr_pull" {
+   principal_id = module.azurerm_kubernetes_cluster.kubelet_identity_object_id["aks1"]
+   role_definition_name = "AcrPull"
+   scope = module.azurerm_container_registry.acr_id["acr1"]
+   skip_service_principal_aad_check = true
+ }
